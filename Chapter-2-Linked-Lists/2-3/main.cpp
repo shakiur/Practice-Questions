68d3630ed4a2e0ec972f8f9e7aa30653e0e8d5ce
Shakiur <shakiur@gmail.com>
2014-10-16 11:50:00 -0400

    Implemented function to remove node from list, given only acces to list (last elem cannot be removed)

--- Chapter-2-Linked-Lists/2-3/main.cpp
+++ Chapter-2-Linked-Lists/2-3/main.cpp
@@ -14,12 +14,19 @@ using namespace std;
 template<class T>
 void display(Node<T>*);
 
+template<class T>
+void removeNode(Node<T>*&);
+
 int main(){
 	Node<char>* e = new Node<char>('e');
 	Node<char>* d = new Node<char>('d', e);
 	Node<char>* c = new Node<char>('c', d);
 	Node<char>* b = new Node<char>('b', c);
 	Node<char>* a = new Node<char>('a', b);
+
+	display(a);
+
+	removeNode(c);
 
 	display(a);
 }
@@ -31,4 +38,20 @@ void display(Node<T>* head){
 		head = head->getNext();
 	}
 	cout << "NULL" << endl;
+}
+
+template<class T>
+void removeNode(Node<T>*& curr){
+
+	Node<T>* next = curr->getNext();
+
+	// Last element cannot be removed
+	if(next){
+		// Swap data
+		curr->setData(next->getData());
+	
+		// Remove next data from linked list
+		curr->setNext(next->getNext());
+	}
+
 }
