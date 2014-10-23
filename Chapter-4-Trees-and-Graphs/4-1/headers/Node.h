9a3bb76ef9d890628807bbb2f6ee2dea323d4708
Shakiur <shakiur@gmail.com>
2014-10-23 00:21:38 -0400

    Implementation of binary tree

---
+++ Chapter-4-Trees-and-Graphs/4-1/headers/Node.h
@@ -0,0 +1,57 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node{
+	int data;
+	Node* left;
+	Node* right;
+
+	public:
+		// Constructors
+		Node(int d) : data(d), left(NULL), right(NULL) {}
+
+		// Getters
+		int getData() { return data; }
+		Node* getLeft() { return left; }
+		Node* getRight() { return right; }
+
+		// Setters
+		void setData(int d) { data = d; }
+		void setLeft(Node* n) { left = n; }
+		void setRight(Node* n) { right = n; }
+
+		void push(int d){
+			Node * n = new Node(d);
+			Node * itr = this;
+
+			while(itr){
+				if(d < itr->getData()){
+					if(itr->getLeft()){
+						itr = itr->getLeft();
+					}
+					else{
+						itr->setLeft(n);
+						break;
+					}
+				}
+				else if(d > itr->getData()){
+					if(itr->getRight()){
+						itr = itr->getLeft();
+					}
+					else{
+						itr->setRight(n);
+						break;
+					}
+				}
+				else	{
+					// d = data
+					break;
+				}
+			}
+
+
+
+		}
+};
+
+#endif
---
+++ Chapter-4-Trees-and-Graphs/4-1/main.cpp
@@ -0,0 +1,15 @@
+/*
+Implement a function to check if a tree is balanced. For the purposes of this question,
+a balanced tree is defined to be a tree such that no two leaf nodes differ in distance
+from the root by more than one
+*/
+
+#include <iostream>
+#include "headers/Node.h"
+using namespace std;
+
+int main(){
+	Node * n = new Node(5);
+	n->push(1);
+	n->push(5);
+}
