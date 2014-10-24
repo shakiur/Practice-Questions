ff8df9f203c615faee110d2bcb241e04b60362bd
Shakiur <shakiur@gmail.com>
2014-10-24 14:06:14 -0400

    Implemeted preorder display function, and function to determine if tree is balanced

--- Chapter-4-Trees-and-Graphs/4-1/main.cpp
+++ Chapter-4-Trees-and-Graphs/4-1/main.cpp
@@ -6,10 +6,62 @@ from the root by more than one
 
 #include <iostream>
 #include "headers/Node.h"
+
+void preOrder(Node*);
+
+int maxDepth(Node*);
+int minDepth(Node*);
+
+bool isBalanced(Node*);
+
 using namespace std;
 
 int main(){
-	Node * n = new Node(5);
-	n->push(1);
+	Node * n = new Node(10);
 	n->push(5);
+	n->push(3);
+	n->push(1);
+	n->push(20);
+
+	//preOrder(n);
+
+	if(isBalanced(n))
+		cout << "Tree is balanced\n";
+	else
+		cout << "Tree is NOT balanced\n";
+}
+
+void preOrder(Node* itr){
+
+	cout << itr->getData() << endl;
+
+	if(itr->getLeft()){
+		cout << "Left: ";
+		preOrder(itr->getLeft());
+	}
+	if(itr->getRight()){
+		cout << "Right: ";
+		preOrder(itr->getRight());
+	}
+
+}
+
+int maxDepth(Node* r){
+	if(r == NULL)
+		return 0;
+	else
+		return 1 + max(maxDepth(r->getRight()), maxDepth(r->getLeft()));
+}
+
+int minDepth(Node* r){
+	if(r == NULL)
+		return 0;
+	else
+		return 1 + min(minDepth(r->getRight()), minDepth(r->getLeft()));
+}
+
+bool isBalanced(Node* r){
+	cout << "Max Depth: " << maxDepth(r) << endl;
+	cout << "Min Depth: " << minDepth(r) << endl;
+	return (maxDepth(r) - minDepth(r) <= 1);
 }
