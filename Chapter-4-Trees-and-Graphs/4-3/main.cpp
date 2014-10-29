4d2f4ee53ddc2ed360b66a5fa88e35f171298c76
Shakiur <shakiur@gmail.com>
2014-10-29 14:13:32 -0400

    Implement better arr to tree function that uses recursion to get middle nodes

--- Chapter-4-Trees-and-Graphs/4-3/main.cpp
+++ Chapter-4-Trees-and-Graphs/4-3/main.cpp
@@ -2,22 +2,45 @@
 Given a sorted (increasing order) array, write an algorithm to create
 a binary tree with minimal height
 */
+
 #include <iostream>
-#include "headers/BTree.h"
+#include "headers/BTNode.h"
+
+BTNode* addNodes(int[], int, int);
+BTNode* buildTree(int[], int);
 
 int main(){
 
-	int arr[] = {1, 2, 3, 4, 5};
-	int size = 4;
+	int size = 7;
+	int arr[] = {1, 2, 3, 4, 5, 6, 7};
 
-	int root = arr[size/2];
-	arr[size/2] = -1;
+	BTNode* root = buildTree(arr, size);
+}
 
-	BTree* n = new BTree(root);
+BTNode* addNodes(int arr[], int begin, int end){
 
-	for(int i = 0; i < size; i++){
-		if(arr[i] != -1)
-			n->add(arr[i]);
+	if(begin <= end){
+		int mid = (begin + end)/2;
+
+		/*
+		std::cout << begin << " | " << end << std::endl;
+		std::cout << "Mid: " << mid << "\n";
+		std::cout << "Val: " << arr[mid] << std::endl << std::endl;
+		*/
+		
+		BTNode * n = new BTNode(arr[mid]);
+
+		n->setLeft(addNodes(arr, begin, mid-1));
+		n->setRight(addNodes(arr, mid+1, end));
+
+		return n;
+	}
+	else{
+		return NULL;
 	}
 
+}
+
+BTNode* buildTree(int arr[], int size){
+	return addNodes(arr, 0, size-1);
 }
