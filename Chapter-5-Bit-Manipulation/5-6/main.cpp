0502f00661a34bf5e0cb8e35002e09a3d9aed16c
Shakiur <shakiur@gmail.com>
2014-11-11 21:49:22 -0500

    Implement function using bitset

--- Chapter-5-Bit-Manipulation/5-6/main.cpp
+++ Chapter-5-Bit-Manipulation/5-6/main.cpp
@@ -3,28 +3,37 @@ Write a program to swap odd and even bits in an integer with as few instructions
 possible (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, etc).
 */
 #include <iostream>
+#include <bitset>
 
 int swap(int);
 
 int main(){
-	swap(234);
+	std::cout << swap(234) << std::endl;
 }
 
 int swap(int x){
-	int y = 0;
+
 	int prev_bit;
 	int cur_bit;
 
-	for(int i = 31; i >= 0; i--){
+	std::bitset<sizeof(x)*8> x_bs(x);
 
-		if(i % 2)
-			prev_bit = ((x >> i) && 1);
+	for(int i = x_bs.size() - 1; i >= 0; i--){
+		if(i % 2){
+			// Odd bit
+			prev_bit = ((x >> i) & 1);
+		}
 		else{
-			cur_bit = ((x >> i) && 1);
+			//Even bit
+			cur_bit = ((x >> i) & 1);
+
+			// Switch bits
+			x_bs.set(i, prev_bit);
+			x_bs.set(i+1, cur_bit);
+
 		}
 	}
 
-	std::cout << std::endl;
+	return (int)x_bs.to_ulong();
 
-	return y;
 }
