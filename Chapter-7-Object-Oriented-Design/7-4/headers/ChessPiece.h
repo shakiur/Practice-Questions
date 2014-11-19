cd66f9c9841b96f4d6b20be278df1e0197680ab4
Shakiur <shakiur@gmail.com>
2014-11-18 19:39:31 -0500

    Add all chess pieces

--- Chapter-7-Object-Oriented-Design/7-4/headers/ChessPiece.h
+++ Chapter-7-Object-Oriented-Design/7-4/headers/ChessPiece.h
@@ -1,6 +1,8 @@
 #ifndef CHESSPIECE_H
 #define CHESSPIECE_H
 
+#include <cmath>
+
 class ChessPiece{
 	protected:
 		std::string name;
@@ -30,9 +32,108 @@ class Pawn : public ChessPiece{
 			ChessPiece(n, a, x, y) {}
 
 		bool validMove(int x, int y){
+			// Checks if piece moves 1 spot forward or 1 spot diagonal
 			return (	(y == (y_pos + 1)) 
 							&& 
 						((x == x_pos) || x == (x_pos + 1)));
+		}
+};
+
+class King : public ChessPiece{
+
+	public: 
+		King(std::string n, char a, int x, int y) :
+			ChessPiece(n, a, x, y) {}
+
+		bool validMove(int x, int y){
+			// Checks if piece moved 1 position
+			return (
+					((x == x_pos) && (y == (y_pos + 1)))
+						||
+					((x == x_pos) && (y == (y_pos - 1)))
+						||
+					((x == (x_pos + 1)) && (y == y_pos))
+						||
+					((x == (x_pos - 1)) && (y == y_pos))
+				);
+		}
+};
+
+class Queen : public ChessPiece{
+
+	public:
+		Queen(std::string n, char a, int x, int y) : 
+			ChessPiece(n, a, x, y) {}
+
+		bool validMove(int x, int y){
+			// Checks if diagonal move was made
+			// or vertical/horizontal move was made
+			return 	(	// Diagnoal
+						( std::abs(x - x_pos) == std::abs(y - y_pos) )
+							||
+						// Vertical
+						( x == x_pos)
+							||
+						// Horizontal
+						( y == y_pos)
+					);
+		}
+};
+
+
+class Bishop : public ChessPiece{
+
+	public:
+		Bishop(std::string n, char a, int x, int y) : 
+			ChessPiece(n, a, x, y) {}
+
+		bool validMove(int x, int y){
+			// Checks if new position is diagonal
+			return std::abs(x - x_pos) == std::abs(y - y_pos);
+		}
+
+};
+
+class Rook : public ChessPiece{
+	public:
+		Rook(std::string n, char a, int x, int y) : 
+			ChessPiece(n, a, x, y) {}
+
+		bool validMove(int x, int y){
+			// Checks if new position is vertical or horizontal
+			return 	(
+						(x == x_pos)
+							||
+						(y == y_pos)
+					);
+		}
+};
+
+class Knight : public ChessPiece{
+
+	public:
+		Knight(std::string n, char a, int x, int y) :
+			ChessPiece(n, a, x, y) {}
+
+		bool validMove(int x, int y){
+			// Checks if new position is L in any direction
+			return 	(
+						(y == (y_pos + 2) && (x == (x_pos + 1)))
+							||
+						(y == (y_pos + 2) && (x == (x_pos - 1)))
+							||
+						(y == (y_pos - 2) && (x == (x_pos + 1)))
+							||
+						(y == (y_pos - 2) && (x == (x_pos - 1)))
+							||
+						(x == (x_pos + 2) && (y == (y_pos + 1)))
+							||
+						(x == (x_pos + 2) && (y == (y_pos - 1)))
+							||
+						(x == (x_pos - 2) && (y == (y_pos + 1)))
+							||
+						(x == (x_pos - 2) && (y == (y_pos - 1)))
+					);
 		}
 };
 
