129745e7bfe4ebf764f7d7eb68e7ce8a69842df9
Shakiur <shakiur@gmail.com>
2014-12-16 22:44:55 -0500

    Sort individual word

--- Chapter-9-Sorting-And-Searching/9-2/main.cpp
+++ Chapter-9-Sorting-And-Searching/9-2/main.cpp
@@ -4,18 +4,22 @@ other.
 */
 #include <iostream>
 
-std::string mergeSortWord(std::string&, int, int);
+void mergeSortWord(std::string&, int, int);
 
-std::string mergeWord(std::string&, int, int, int, int);
+void mergeWord(std::string&, int, int, int, int);
 
 int main(){
 
 	std::string arr[5] = {"abc", "def", "ghi", "cab", "hig"};
 
+	std::string word = "cabhvnm";
+
+	mergeSortWord(word, 0, word.size()-1);
+
 }
 
 
-std::string mergeSortWord(std::string& word, int beg, int end){
+void mergeSortWord(std::string& word, int beg, int end){
 	if(end > beg){
 		int mid = (beg + end)/2;
 
@@ -28,10 +32,41 @@ std::string mergeSortWord(std::string& word, int beg, int end){
 }
 
 
-std::string mergeWord(std::string& word, int beg1, int end1, int beg2, int end2){
+void mergeWord(std::string& word, int beg1, int end1, int beg2, int end2){
 
 	std::string new_word;
-	int pos = 0;
 
+	int pos = 0;
+	int size = (end2 - beg1) + 1;
+
+	int init_beg1 = beg1;
+
+	// Go through each letter in both halves until entire new word is organized
+	while(pos < size){
+		if(beg1 <= end1 && beg2 <= end2){
+			// Both halves are not completely visited
+			if(int(word[beg1]) < int(word[beg2])){
+				new_word += word[beg1];
+				beg1++;
+			}
+			else{
+				new_word += word[beg2];
+				beg2++;
+			}
+		}
+		else if(beg1 > end1 && beg2 <= end2){
+			// Word 1 has been completely visited, only use word 2
+			new_word += word[beg2];
+			beg2++;
+		}
+		else{
+			// Word 2 has been completely visited, only use word 1
+			new_word += word[beg1];
+			beg1++;
+		}
+		pos++;
+	}
+
+	word = word.substr(0, init_beg1) + new_word + word.substr(end2+1);
 
 }
