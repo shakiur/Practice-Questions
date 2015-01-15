d0c515b9350a84b9215462c432e809d92df4afdb
Shakiur <shakiur@gmail.com>
2015-01-15 17:12:02 -0500

    Initial commit

---
+++ Chapter-9-Sorting-And-Searching/9-5/main.cpp
@@ -0,0 +1,14 @@
+/*
+Given a sorted array of strings which is interspersed with empty strings, write a method
+to find the location of a given string.
+Example: find “ball” in [“at”, “”, “”, “”, “ball”, “”, “”, “car”, “”, “”, “dad”, “”, “”] will return 4
+Example: find “ballcar” in [“at”, “”, “”, “”, “”, “ball”, “car”, “”, “”, “dad”, “”, “”] will return -1
+*/
+#include <iostream>
+
+int main(){
+	std::string arr1[13] = {"at", "","","","ball","","","car","","","dad","",""};
+	std::string arr2[12] = {"at", "", "", "", "", "ball", "car", "", "", "dad", "", ""};
+
+
+}
