25d25e89cbf2d560d602e26944d49e2f98db5019
Shakiur <shakiur@gmail.com>
2015-01-16 13:28:08 -0500

    Function completed

--- Chapter-9-Sorting-And-Searching/9-5/main.cpp
+++ Chapter-9-Sorting-And-Searching/9-5/main.cpp
@@ -14,28 +14,75 @@ int main(){
 	std::string arr1[13] = {"at", "","","","ball","","","car","","","dad","",""};
 	std::string arr2[12] = {"at", "", "", "", "", "ball", "car", "", "", "dad", "", ""};
 
+	std::cout << binarySearchStringArr(arr1, 12, "ball") << std::endl;
+	std::cout << binarySearchStringArr(arr1, 11, "ballcar") << std::endl;
 
+	/*
+	std::string test1 = "e";
+	std::string test2 = "cat";
+
+	if(isStringLessThan(test1, test2))
+		std::cout << test1 << " is less than " << test2 << "\n";
+	else
+		std::cout << test1 << " is greater than " << test2 << "\n";
+	*/
 }
 
 int binarySearchStringArr(std::string* arr, int end, std::string needle){
 
 	int beg = 0;
+	int diff = 1;
+	bool pos_diff = true;
+
+	int mid = (beg + end)/2;
 
 	while(beg <= end){
-
-		int mid = (beg + end)/2;
+		
+		/*
+		// DEBUG INFO
+		std::cout << "Val: " << needle << std::endl;
+		std::cout << "Beg[" << beg << "]: " << arr[beg] << std::endl;
+		std::cout << "Mid[" << mid << "]: " << arr[mid] <<std::endl;
+		std::cout << "End[" << end << "]: " << arr[end] <<std::endl << std::endl;
+		*/
 
 		if(arr[mid] == needle){
 			return mid;
 		}
 		else if(arr[beg] == ""){
 			beg++;
+
+			mid = (beg + end)/2;
 		}
 		else if(arr[end] == ""){
 			end--;
+
+			mid = (beg + end)/2;
 		}
 		else if(arr[mid] == ""){
-			// Figure out case if arr[mid] is "" because then you cant compare it
+			// Keep searching left/right until non-blank is found
+			if(pos_diff){
+				mid = mid + diff;
+				diff++;
+
+				pos_diff = false;
+			}
+			else{
+				mid = mid - diff;
+				diff++;
+
+				pos_diff = true;
+			}
+		}
+		else if(isStringLessThan(needle, arr[mid])){
+			end = mid - 1;
+
+			mid = (beg + end)/2;
+		}
+		else{
+			beg = mid + 1;
+
+			mid = (beg + end)/2;
 		}
 			
 	}
@@ -52,13 +99,22 @@ bool isStringLessThan(std::string word1, std::string word2){
 
 	while(pos1 < word1.size() && pos2 < word2.size()){
 
-		if( int(word1[pos1]) < int(word2[pos2]) )
+		if( int(word1[pos1]) > int(word2[pos2]) ){
+			// Current word1 letter is greater
+			return false;
+		}
+		else if( int(word1[pos1]) < int(word2[pos2])){
+			// Current word1 letter is less
 			return true;
-
-		pos1++;
-		pos2++;
+		}
+		else{
+			// Both letters are equal
+			pos1++;
+			pos2++;
+		}
 	}
 
+	// Word 1 is exact same as word2 except shorter, so its less
 	if(pos1 == word1.size())
 		return true;
 	else
